702bf11906e53588de3a00dfd1346713bfc9dc87
UMFT-2 <UMFT-2@UMFT.AD>
2025-10-08 16:56:06 +0500

    2 mashxulot xatosi bor

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI,  Query
+from fastapi import FastAPI, Query
 from pydantic import BaseModel, Field
-app = FastAPI(title="UniMarket", 
-version="0.1.0") 
-@app.get("/health") 
-def health(): 
+
+app = FastAPI(title="UniMarket", version="0.1.0")
+
+
+@app.get("/health")
+def health():
     return {"status": "ok ya inoyatov akobir"}
+
+
 class HelloResponse(BaseModel):
- message: str = Field(..., examples=["Hello, Alice!"])
+    message: str = Field(..., examples=["Hello, Alice!"])
+
+
 @app.get("/hello", response_model=HelloResponse)
 def hello(name: str = Query("world", min_length=1, max_length=50)):
- return HelloResponse(message=f"Hello, {name}!")
-@app.get("/") 
-def root(): 
+    return HelloResponse(message=f"Hello, {name}!")
+
+
+@app.get("/")
+def root():
     return {"Это сайт": "ok Этот сайт работает"}
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,11 +1,17 @@
 from fastapi.testclient import TestClient
+
 from app.main import app
+
 client = TestClient(app)
+
+
 def test_hello_default():
- r = client.get("/hello")
- assert r.status_code == 200
- assert r.json()["message"] == "Hello, world!"
+    r = client.get("/hello")
+    assert r.status_code == 200
+    assert r.json()["message"] == "Hello, world!"
+
+
 def test_hello_named():
- r = client.get("/hello?name=Alice")
- assert r.status_code == 200
- assert r.json()["message"] == "Hello, Alice!"
+    r = client.get("/hello?name=Alice")
+    assert r.status_code == 200
+    assert r.json()["message"] == "Hello, Alice!"
